7994466db55542b5308e1d54eb1b7b6ef9cba42f
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-05-18 21:07:38 +0900

    cloudinary no npm

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,30 +1,29 @@
-import { v2 as cloudinary } from "npm:cloudinary";
 import { encode } from "https://deno.land/std@0.97.0/encoding/base64.ts";
+import { Sha1 } from "https://deno.land/std@0.119.0/hash/sha1.ts";
 
+// no-npm
+// https://cloudinary.com/documentation/image_upload_api_reference
 export async function uploadToCloudinary(body: Uint8Array) {
-  cloudinary.config({
-    cloud_name: Deno.env.get("CLOUDINARY_CLOUD_NAME")!,
-    api_key: Deno.env.get("CLOUDINARY_API_KEY")!,
-    api_secret: Deno.env.get("CLOUDINARY_API_SECRET")!
+  const cloudName = Deno.env.get("CLOUDINARY_CLOUD_NAME")!;
+  const apiKey = Deno.env.get("CLOUDINARY_API_KEY")!;
+  const apiSecret = Deno.env.get("CLOUDINARY_API_SECRET")!;
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+  const timestamp = String(Math.trunc(new Date().getTime() / 1000));
+  const signature = new Sha1().update(`timestamp=${timestamp}${apiSecret}`).toString();
+
+  const formData = new FormData();
+  formData.append("file", "data:image/png;base64," + encode(body));
+  formData.append("timestamp", timestamp);
+  formData.append("api_key", apiKey);
+  formData.append("signature", signature);
+
+  const result = await fetch(url, {
+    method: "POST",
+    body: formData
   });
-
-  // バイナリデータをCloudinaryにアップしたい
-  // https://cloudinary.com/documentation/node_image_and_video_upload
-  // The file to upload can be specified as a local path, a remote HTTP or HTTPS URL, a whitelisted storage bucket (S3 or Google Storage) URL, a base64 data URI, or an FTP URL. For details, see File source options.
-  //image.data;
-
-  // おくれる形式
-  // https://cloudinary.com/documentation/upload_images#file_source_options
-
-  // const res = await cloudinary.uploader.upload('5.png', {public_id: "denokun"});
-
-  // https://cloudinary.com/documentation/upload_images#upload_via_a_base_64_data_uri
-  // no larger than 100 MB
-  const result = await cloudinary.uploader.upload("data:image/png;base64," + encode(body));
-  console.log(result.secure_url);
-
-  // テスト
-  return result.secure_url;
+  const json = await result.json();
+  return json.secure_url;
 }
 
 if (import.meta.main) {
